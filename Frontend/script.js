538e913abcb4510beefcf081960958d810c861f9
Peanshupranjal18 <pprnch@gmail.com>
2024-08-18 11:25:56 +0530

    Cropcare

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,91 +1,96 @@
 // Function to clear the page content
 function clearPage() {
-    document.getElementById('cityName').textContent = '';
-    document.getElementById('errorContainer').innerHTML = '';
-    document.getElementById('irrigationPattern').textContent = ' ';
-    document.getElementById('oneDayTableContainer').innerHTML = '';
+  document.getElementById("cityName").textContent = "";
+  document.getElementById("errorContainer").innerHTML = "";
+  document.getElementById("irrigationPattern").textContent = " ";
+  document.getElementById("oneDayTableContainer").innerHTML = "";
 }
 
 // Function to handle form submission
 function submitForm(event) {
-    if (event) {
-        event.preventDefault(); // Prevent form submission
-    }
+  if (event) {
+    event.preventDefault(); // Prevent form submission
+  }
 
-    // Clear the page
-    clearPage();
+  // Clear the page
+  clearPage();
 
-    // Get form values
-    const latitude = document.getElementById('latitude').value;
-    const longitude = document.getElementById('longitude').value;
-    const cropType = document.getElementById('cropType').value;
+  // Get form values
+  const latitude = document.getElementById("latitude").value;
+  const longitude = document.getElementById("longitude").value;
+  const cropType = document.getElementById("cropType").value;
 
-    // Send form data to the server
-    axios.get('http://localhost:3000/weather', {
-        params: {
-            latitude: latitude,
-            longitude: longitude,
-            cropType: cropType
-        }
+  // Send form data to the server
+  axios
+    .get("http://localhost:3000/weather", {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+        cropType: cropType,
+      },
     })
-    .then(response => {
-        // Handle response from server (weather data)
-        const weatherData = response.data;
-        const irrigationPattern = analyzeForecast(weatherData);
-        displayIrrigationPattern(irrigationPattern);
-        displayWeatherData(weatherData, latitude, longitude);
-        createWeatherChart(weatherData); // Add weather chart visualization
+    .then((response) => {
+      // Handle response from server (weather data)
+      const weatherData = response.data;
+      const irrigationPattern = analyzeForecast(weatherData);
+      displayIrrigationPattern(irrigationPattern);
+      displayWeatherData(weatherData, latitude, longitude);
+      createWeatherChart(weatherData); // Add weather chart visualization
     })
-    .catch(error => {
-        console.error('Error fetching weather data:', error);
-        displayError('Error fetching weather data. Please try again later.');
+    .catch((error) => {
+      console.error("Error fetching weather data:", error);
+      displayError("Error fetching weather data. Please try again later.");
     });
 }
 
 // Function to display weather data on the webpage
 function displayWeatherData(weatherData, latitude, longitude) {
-    const oneDayTableContainer = document.getElementById('oneDayTableContainer');
-    const cityName = document.getElementById('cityName');
+  const oneDayTableContainer = document.getElementById("oneDayTableContainer");
+  const cityName = document.getElementById("cityName");
 
-    // Clear previous weather data, if any
-    oneDayTableContainer.innerHTML = '';
-    cityName.innerHTML = '';
+  // Clear previous weather data, if any
+  oneDayTableContainer.innerHTML = "";
+  cityName.innerHTML = "";
 
-    // Display city name
-    axios.get(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`)
-    .then(response => {
-        const city = response.data.locality;
-        cityName.textContent = `Forecast for ${city}`;
+  // Display city name
+  axios
+    .get(
+      `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=en`
+    )
+    .then((response) => {
+      const city = response.data.locality;
+      cityName.textContent = `Forecast for ${city}`;
     })
-    .catch(error => {
-        console.error('Error fetching city name:', error);
-        displayError('Error fetching city name. Please try again later.');
+    .catch((error) => {
+      console.error("Error fetching city name:", error);
+      displayError("Error fetching city name. Please try again later.");
     });
 
-    const table = createWeatherTable(weatherData);
-    const scrollableContainer = document.createElement('div');
-    scrollableContainer.classList.add('table-responsive', 'scrollable-table');
-    scrollableContainer.appendChild(table);
-    oneDayTableContainer.appendChild(scrollableContainer);
+  const table = createWeatherTable(weatherData);
+  const scrollableContainer = document.createElement("div");
+  scrollableContainer.classList.add("table-responsive", "scrollable-table");
+  scrollableContainer.appendChild(table);
+  oneDayTableContainer.appendChild(scrollableContainer);
 }
 
 // Function to display irrigation pattern suggestion
 function displayIrrigationPattern(irrigationPattern) {
-    const irrigationPatternContainer = document.getElementById('irrigationPattern');
-    if (irrigationPatternContainer) {
-        irrigationPatternContainer.textContent = `Recommended Irrigation Pattern: ${irrigationPattern}`;
-    } else {
-        console.error('Irrigation pattern container not found in the DOM.');
-    }
+  const irrigationPatternContainer =
+    document.getElementById("irrigationPattern");
+  if (irrigationPatternContainer) {
+    irrigationPatternContainer.textContent = `Recommended Irrigation Pattern: ${irrigationPattern}`;
+  } else {
+    console.error("Irrigation pattern container not found in the DOM.");
+  }
 }
 
 // Function to create a weather table
 function createWeatherTable(weatherData) {
-    const table = document.createElement('table');
-    table.classList.add('table');
+  const table = document.createElement("table");
+  table.classList.add("table");
 
-    const tableHeader = document.createElement('thead');
-    tableHeader.innerHTML = `
+  const tableHeader = document.createElement("thead");
+  tableHeader.innerHTML = `
         <tr>
             <th>Time</th>
             <th>Temperature (°C)</th>
@@ -105,13 +110,13 @@ function createWeatherTable(weatherData) {
             <th>Soil Moisture (27-81 cm)</th>
         </tr>
     `;
-    table.appendChild(tableHeader);
+  table.appendChild(tableHeader);
 
-    // Create table body
-    const tableBody = document.createElement('tbody');
-    weatherData.hourly.time.forEach((time, index) => {
-        const row = document.createElement('tr');
-        row.innerHTML = `
+  // Create table body
+  const tableBody = document.createElement("tbody");
+  weatherData.hourly.time.forEach((time, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
             <td>${formatDateTime(time)}</td>
             <td>${weatherData.hourly.temperature_2m[index]}</td>
             <td>${weatherData.hourly.relative_humidity_2m[index]}</td>
@@ -129,152 +134,176 @@ function createWeatherTable(weatherData) {
             <td>${weatherData.hourly.soil_moisture_9_to_27cm[index]}</td>
             <td>${weatherData.hourly.soil_moisture_27_to_81cm[index]}</td>
         `;
-        tableBody.appendChild(row);
-    });
-    table.appendChild(tableBody);
+    tableBody.appendChild(row);
+  });
+  table.appendChild(tableBody);
 
-    return table;
+  return table;
 }
 
 // Function to format date and time
 function formatDateTime(dateTime) {
-    const date = new Date(dateTime);
-    const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-    return formattedDate;
+  const date = new Date(dateTime);
+  const formattedDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+  return formattedDate;
 }
 
 // Function to display error message
 function displayError(message) {
-    clearPage(); // Clear the page
-    const errorMessage = document.createElement('p');
-    errorMessage.textContent = message;
-    errorMessage.classList.add('text-danger');
-    document.getElementById('errorContainer').appendChild(errorMessage);
+  clearPage(); // Clear the page
+  const errorMessage = document.createElement("p");
+  errorMessage.textContent = message;
+  errorMessage.classList.add("text-danger");
+  document.getElementById("errorContainer").appendChild(errorMessage);
 }
 
 // Function to analyze weather forecast data and suggest irrigation pattern
 function analyzeForecast(weatherData) {
-    // Get relevant weather data
-    const precipitation = weatherData.hourly.precipitation_probability; 
-    const temperature = weatherData.hourly.temperature_2m; 
-    const humidity = weatherData.hourly.relative_humidity_2m; 
-    const soilMoisture = weatherData.hourly.soil_moisture_0_to_1cm; 
+  // Get relevant weather data
+  const precipitation = weatherData.hourly.precipitation_probability;
+  const temperature = weatherData.hourly.temperature_2m;
+  const humidity = weatherData.hourly.relative_humidity_2m;
+  const soilMoisture = weatherData.hourly.soil_moisture_0_to_1cm;
 
-    // Define irrigation patterns and their conditions
-    const irrigationPatterns = [
-        { pattern: "Biweekly irrigation (every 3 days)", 
-          condition: () => precipitation.every(value => value < 20) &&
-                         temperature.every(value => value > 25) &&
-                         humidity.every(value => value < 70) &&
-                         soilMoisture.every(value => value < 50) 
-        },
-        { pattern: "Weekly irrigation (every 7 days)", 
-          condition: () => precipitation.every(value => value < 30) &&
-                         temperature.every(value => value > 20) &&
-                         humidity.every(value => value < 60) &&
-                         soilMoisture.every(value => value < 60) 
-        },
-        { pattern: "Twice a week irrigation (every 3-4 days)", 
-          condition: () => precipitation.every(value => value < 40) &&
-                         temperature.every(value => value > 18) &&
-                         humidity.every(value => value < 65) &&
-                         soilMoisture.every(value => value < 70) 
-        },
-    ];
-    const suggestedPattern = irrigationPatterns.find(pattern => pattern.condition());
-    if (suggestedPattern) {
-        return suggestedPattern.pattern;
-    } else {
-        return "Standard irrigation pattern";
-    }
+  // Define irrigation patterns and their conditions
+  const irrigationPatterns = [
+    {
+      pattern: "Biweekly irrigation (every 3 days)",
+      condition: () =>
+        precipitation.every((value) => value < 20) &&
+        temperature.every((value) => value > 25) &&
+        humidity.every((value) => value < 70) &&
+        soilMoisture.every((value) => value < 50),
+    },
+    {
+      pattern: "Weekly irrigation (every 7 days)",
+      condition: () =>
+        precipitation.every((value) => value < 30) &&
+        temperature.every((value) => value > 20) &&
+        humidity.every((value) => value < 60) &&
+        soilMoisture.every((value) => value < 60),
+    },
+    {
+      pattern: "Twice a week irrigation (every 3-4 days)",
+      condition: () =>
+        precipitation.every((value) => value < 40) &&
+        temperature.every((value) => value > 18) &&
+        humidity.every((value) => value < 65) &&
+        soilMoisture.every((value) => value < 70),
+    },
+  ];
+  const suggestedPattern = irrigationPatterns.find((pattern) =>
+    pattern.condition()
+  );
+  if (suggestedPattern) {
+    return suggestedPattern.pattern;
+  } else {
+    return "Standard irrigation pattern";
+  }
 }
 
 // Function to create weather chart
 function createWeatherChart(weatherData) {
-    const existingChart = Chart.getChart('weatherChart');
-    if (existingChart) {
-        existingChart.destroy();
-    }
-    const ctx = document.getElementById('weatherChart').getContext('2d');
-    const timeLabels = weatherData.hourly.time.map(time => formatDateTime(time));
-    const temperatureData = weatherData.hourly.temperature_2m;
-    const humidityData = weatherData.hourly.relative_humidity_2m;
-    const precipitationData = weatherData.hourly.precipitation_probability;
+  const existingChart = Chart.getChart("weatherChart");
+  if (existingChart) {
+    existingChart.destroy();
+  }
+  const ctx = document.getElementById("weatherChart").getContext("2d");
+  const timeLabels = weatherData.hourly.time.map((time) =>
+    formatDateTime(time)
+  );
+  const temperatureData = weatherData.hourly.temperature_2m;
+  const humidityData = weatherData.hourly.relative_humidity_2m;
+  const precipitationData = weatherData.hourly.precipitation_probability;
 
-    const weatherChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: timeLabels,
-            datasets: [{
-                label: 'Temperature (°C)',
-                yAxisID: 'temperature',
-                borderColor: 'rgb(255, 99, 132)',
-                data: temperatureData
-            }, {
-                label: 'Humidity (%)',
-                yAxisID: 'humidity',
-                borderColor: 'rgb(54, 162, 235)',
-                data: humidityData
-            }, {
-                label: 'Precipitation Probability (%)',
-                yAxisID: 'precipitation',
-                borderColor: 'rgb(75, 192, 192)',
-                data: precipitationData
-            }]
+  const weatherChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: timeLabels,
+      datasets: [
+        {
+          label: "Temperature (°C)",
+          yAxisID: "temperature",
+          borderColor: "rgb(255, 99, 132)",
+          data: temperatureData,
         },
-        options: {
-            scales: {
-                yAxes: [{
-                    id: 'temperature',
-                    type: 'linear',
-                    position: 'left',
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Temperature (°C)'
-                    }
-                }, {
-                    id: 'humidity',
-                    type: 'linear',
-                    position: 'right',
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Humidity (%)'
-                    }
-                }, {
-                    id: 'precipitation',
-                    type: 'linear',
-                    position: 'right',
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'Precipitation Probability (%)'
-                    }
-                }]
-            }
-        }
-    });
+        {
+          label: "Humidity (%)",
+          yAxisID: "humidity",
+          borderColor: "rgb(54, 162, 235)",
+          data: humidityData,
+        },
+        {
+          label: "Precipitation Probability (%)",
+          yAxisID: "precipitation",
+          borderColor: "rgb(75, 192, 192)",
+          data: precipitationData,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            id: "temperature",
+            type: "linear",
+            position: "left",
+            scaleLabel: {
+              display: true,
+              labelString: "Temperature (°C)",
+            },
+          },
+          {
+            id: "humidity",
+            type: "linear",
+            position: "right",
+            scaleLabel: {
+              display: true,
+              labelString: "Humidity (%)",
+            },
+          },
+          {
+            id: "precipitation",
+            type: "linear",
+            position: "right",
+            scaleLabel: {
+              display: true,
+              labelString: "Precipitation Probability (%)",
+            },
+          },
+        ],
+      },
+    },
+  });
 }
 
 // Function to get user's location
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-            position => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                document.getElementById('latitude').value = latitude;
-                document.getElementById('longitude').value = longitude;
-            },
-            error => {
-                console.error('Error getting location:', error);
-                displayError('Error getting location. Please try again or enter manually.');
-            }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        document.getElementById("latitude").value = latitude;
+        document.getElementById("longitude").value = longitude;
+      },
+      (error) => {
+        console.error("Error getting location:", error);
+        displayError(
+          "Error getting location. Please try again or enter manually."
         );
-    } else {
-        console.error('Geolocation is not supported by this browser.');
-        displayError('Geolocation is not supported by this browser.');
-    }
+      }
+    );
+  } else {
+    console.error("Geolocation is not supported by this browser.");
+    displayError("Geolocation is not supported by this browser.");
+  }
 }
 
 // Add event listeners
-document.getElementById('getLocationBtn').addEventListener('click', getLocation);
-document.getElementById('farmDetailsForm').addEventListener('submit', submitForm);
+document
+  .getElementById("getLocationBtn")
+  .addEventListener("click", getLocation);
+document
+  .getElementById("farmDetailsForm")
+  .addEventListener("submit", submitForm);
--- README.md
+++ README.md
@@ -1,35 +1,35 @@
 # CropCare
 
-CropCare is a web application designed to provide farmers with essential weather forecast information and irrigation pattern suggestions, aiding in optimized crop management. Developed as the final project submission for GHW-Data, CropCare aims to enhance agricultural practices through data-driven insights.
+CropCare is a web application designed to provide farmers with essential weather forecast information and irrigation pattern suggestions, aiding in optimized crop management. CropCare aims to enhance agricultural practices through data-driven insights.
 
 ## Features
 
 - **Weather Forecast**: Displays a comprehensive weather forecast, including temperature, humidity, and precipitation probability.
-- **Irrigation Pattern Suggestions**: Analyzes forecast data to suggest irrigation patterns based on predefined conditions. (*Further enhancements needed for crop-specific recommendations*)
+- **Irrigation Pattern Suggestions**: Analyzes forecast data to suggest irrigation patterns based on predefined conditions. (_Further enhancements needed for crop-specific recommendations_)
 - **Customizable Crop Selection**: Users can choose their crop type, though it currently doesn't influence functionality.
 
 ## Technologies Used
 
 - **Frontend**: HTML, CSS, JavaScript (with Axios for HTTP requests, and Chart.js for visualization)
-- **Backend**: Node.js (server.js not included in this repository)
+- **Backend**: Node.js
 - **External APIs**: OpenWeather API (weather data), BigDataCloud API (reverse geocoding)
 - **Development Tools**: Visual Studio Code, Git
 
 ## Usage
 
-1. **Clone the repository**: 
-    ```bash
-    git clone https://github.com/Loskoss/Cropcare.git
-    ```
-2. **Navigate to the project directory**: 
-    ```bash
-    cd CropCare
-    ```
-3. **Start the backend server**: 
-    ```bash
-    node backend/server.js
-    ```
-    Ensure Node.js is installed and dependencies are installed via npm or yarn.
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/Loskoss/Cropcare.git
+   ```
+2. **Navigate to the project directory**:
+   ```bash
+   cd CropCare
+   ```
+3. **Start the backend server**:
+   ```bash
+   node backend/server.js
+   ```
+   Ensure Node.js is installed and dependencies are installed via npm or yarn.
 4. **Open `index.html` in a web browser**.
 5. **Enter the latitude and longitude** of the farm location.
 6. **Select the crop type** from the dropdown menu (not currently utilized).
@@ -44,7 +44,3 @@ CropCare is a web application designed to provide farmers with essential weather
 3. **Email Alerts**: Provide email notifications for weather forecast changes and irrigation recommendations.
 4. **Historical Data Analysis**: Analyze historical weather data to offer long-term farming insights and trends.
 5. **Crop Suggestion** : suggest crop based on historical data.
-
-## Contributing
-
-Contributions, suggestions, feature requests, and bug reports are encouraged. Please open an issue or submit a pull request.
